4779421020118a8be2280e2db6114348317d9086
Pap Áron <paronka04@gmail.com>
2026-02-20 23:54:17 +0100

    update: matrix_utils.py instead of printing output, it now returns it

--- matrix_utils.py
+++ matrix_utils.py
@@ -32,3 +32,15 @@ def matrix_transpose(matrix: np.ndarray) -> np.ndarray:
         for j in range(matrix.shape[1]):
             result[j, i] = matrix[i, j]
     return result
+
+
+def matrix_mult(matrix1: np.ndarray, matrix2: np.ndarray) -> np.ndarray:
+    if (matrix1.shape[1] != matrix2.shape[0]):
+        raise ValueError(f"Wrong shape of the matrices{matrix1.shape}, {matrix2.shape}")
+    
+    result = np.zeros((matrix1.shape[0], matrix2.shape[1]))
+    for i in range(result.shape[0]):
+        for j in range(result.shape[1]):
+            for k in range(matrix1.shape[1]):
+                result[i, j] += matrix1[i, k] * matrix2[k, j]
+    return result
