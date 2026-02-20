855f8b60bdea64ec725a02ee14f987d6e89003d4
Pap Áron <paronka04@gmail.com>
2026-02-20 23:33:11 +0100

    added matrix_transposition.py which includes the matrix_transopsition function

---
+++ matrix_transposition.py
@@ -0,0 +1,15 @@
+import matrix_utils
+import numpy as np
+
+def matrix_transposition(matrix: np.ndarray) -> np.ndarray:
+    result = np.zeros((matrix.shape[1], matrix.shape[0]))
+    print(matrix.shape, result.shape)
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            result[j, i] = matrix[i, j]
+    return result
+
+
+
+sample = np.array([[1,2,3], [4,5,6]])
+matrix_transposition(sample)
