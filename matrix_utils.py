cd3b784b68e5498ae3ad224cca9442b4fedbef43
Pap Áron <paronka04@gmail.com>
2026-02-20 23:15:52 +0100

    added matrix_scalar_mult function to matrix_utils.py

--- matrix_utils.py
+++ matrix_utils.py
@@ -7,6 +7,10 @@ def print_matrix(matrix: np.ndarray) -> None:
             print(matrix[i, j], end=" ")
         print("] ")
 
-
-sample = np.array([[1,2,3],[4,5,6],[7,8,9]])
-print_matrix(sample)
+        
+def matrix_scalar_mult(matrix: np.ndarray, value: int) -> np.ndarray:
+    result = np.zeros(matrix.shape)
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            result[i, j] = matrix[i, j] * value
+    return result
